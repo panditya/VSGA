93a1d59263565aed569dc99b26abbb14adbf0b56
Panditya Agrisyah Putra <panditya25@gmail.com>
2019-07-08 11:23:18 +0700

    added menu & show the toast

--- app/src/main/java/com/panditya/vsga/MainActivity.java
+++ app/src/main/java/com/panditya/vsga/MainActivity.java
@@ -4,8 +4,12 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,5 +33,22 @@ public class MainActivity extends AppCompatActivity {
                 isClicked = !isClicked;
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.option_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.profileMenuItem) {
+            Toast.makeText(this, "Hmm nothing to show about your profile", Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
