8a1fe102501fe94b33a9c842e9e1829d7efbc6e9
Panditya Agrisyah Putra <panditya25@gmail.com>
2019-07-08 11:06:20 +0700

    handle button onClick

--- app/src/main/java/com/panditya/vsga/MainActivity.java
+++ app/src/main/java/com/panditya/vsga/MainActivity.java
@@ -1,7 +1,33 @@
 package com.panditya.vsga;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    public boolean isClicked = false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button okButton = findViewById(R.id.button);
+
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("buttonOnClickHandled!", "well hello people!");
+                if (isClicked == true) {
+                    okButton.setText("Hohoho ya found me");
+                } else {
+                    okButton.setText("I tho ya know it");
+                }
+                isClicked = !isClicked;
+            }
+        });
+    }
 }
