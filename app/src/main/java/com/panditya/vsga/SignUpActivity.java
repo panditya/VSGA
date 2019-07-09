80c530bb831dee1b3dc45c4585656c808f1f2ec7
Panditya Agrisyah Putra <panditya25@gmail.com>
2019-07-09 11:06:39 +0700

    added auth screens

--- app/src/main/java/com/panditya/vsga/SignUpActivity.java
+++ app/src/main/java/com/panditya/vsga/SignUpActivity.java
@@ -6,12 +6,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class SignUpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_up);
 
     }
 }
