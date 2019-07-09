14a118eb3d947cdb145503f1d3084d8d11414222
Panditya Agrisyah Putra <panditya25@gmail.com>
2019-07-09 10:08:13 +0700

    redesigned main activity layout

--- app/src/main/java/com/panditya/vsga/MainActivity.java
+++ app/src/main/java/com/panditya/vsga/MainActivity.java
@@ -1,61 +1,12 @@
 package com.panditya.vsga;
 
-import android.content.Intent;
-import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    public boolean isClicked = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        final Button okButton = findViewById(R.id.button);
-
-        okButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d("buttonOnClickHandled!", "well hello people!");
-                if (isClicked == true) {
-                    okButton.setText("Hohoho ya found me");
-                } else {
-                    okButton.setText("I tho ya know it");
-                }
-                isClicked = !isClicked;
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.option_menu, menu);
-
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.profileMenuItem:
-                Intent intent = new Intent(this, ProfileActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.signOutMenuItem:
-                Toast.makeText(this, "Hmm nothing to show about your profile", Toast.LENGTH_SHORT).show();
-                break;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
